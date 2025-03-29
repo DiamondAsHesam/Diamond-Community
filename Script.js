0cc6e90e64e58874c7cd8aecc271f3992a7059c6
DiamondAsHesam <99635385+DiamondAsHesam@users.noreply.github.com>
2025-03-29 23:54:13 +0330

    Script.js

--- Script.js
+++ Script.js
@@ -1,10 +1,3 @@
-// مثال برای یک انیمیشن ساده
 document.addEventListener('DOMContentLoaded', function() {
-    const hero = document.querySelector('.hero');
-    hero.style.opacity = 0;
-    
-    setTimeout(() => {
-        hero.style.transition = 'opacity 2s';
-        hero.style.opacity = 1;
-    }, 500);
+    console.log("سایت با موفقیت بارگذاری شد!");
 });

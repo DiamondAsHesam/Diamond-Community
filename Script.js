f77706f8c9a66614d5e3e88d0ee2fe1116549f92
DiamondAsHesam <99635385+DiamondAsHesam@users.noreply.github.com>
2025-03-30 00:05:38 +0330

    Script.js

--- Script.js
+++ Script.js
@@ -1,3 +1,4 @@
+// JavaScript simple functionality to ensure the site is loaded correctly
 document.addEventListener('DOMContentLoaded', function() {
     console.log("سایت با موفقیت بارگذاری شد!");
 });
